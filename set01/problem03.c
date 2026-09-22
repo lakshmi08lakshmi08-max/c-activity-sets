71ca957da716ea98daf1bb9bfc217b41ae4cdac7
lakshmi08lakshmi08-max <lakshmi08lakshmi08@gmail.com>
2026-09-22 22:15:49 +0530

    Update problem03.c

---
+++ set01/problem03.c
@@ -0,0 +1,25 @@
+//Write a program to add two numbers using four functions
+#include <stdio.h>
+
+int input()
+{  int n;
+    scanf("%d", &n);
+  return n;
+}
+int add(int a, int b)
+{  return a + b;
+}
+void output(int r)
+{
+  printf("Sum of two numbers = %d", r);
+}
+int main()
+{
+    int a, b, s;
+    printf("Enter two numbers:");
+    a = input();
+    b = input();
+    s = add(a, b);
+    output(s);
+    return 0;
+}
