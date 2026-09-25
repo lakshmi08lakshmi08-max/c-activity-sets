3e50bcbf934add7f2ce1ec5a96b9dbd75c01f3c8
lakshmi08lakshmi08-max <lakshmi08lakshmi08@gmail.com>
2026-09-25 15:58:49 +0530

    Update problem05.c

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int input_size()
+{
+    int n;
+    printf("Enter the array size\n");
+    scanf("%d",&n);
+    return n;
+}
+
+void input_numbers(int n, int a[n])
+{
+    printf("enter numbers:");
+    for(int i=0;i<n;i++)
+    {
+        printf("enter number %d:",i);
+        scanf("%d",&a[i]);
+    }
+}
